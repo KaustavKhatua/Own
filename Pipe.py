cbd5ee8ce9e2e955a3348d6b8f8aef7dc17fb5cc
KaustavKhatua <64597251+KaustavKhatua@users.noreply.github.com>
2020-11-04 02:25:26 +0530

    Add files via upload

---
+++ Pipe.py
@@ -0,0 +1,25 @@
+import os, time 
+def child(pipeout):     
+	zzz = 1     
+	while 1:        
+		print('A')
+		# print('Entered child')
+		time.sleep(2)                          # make parent wait
+		os.write(pipeout, bytes(f'{zzz}', 'utf-8'))     # send to parent       
+		print('Written to Pipeline')  
+		zzz = (zzz+1)                        # goto 0 after 4 
+
+def parent( ):     
+	pipein, pipeout = os.pipe( )                  # make 2-ended pipe     
+	if os.fork( ) == 0:                           # copy this process         
+		child(pipeout)                             # in copy, run child     
+	else:                                         # in parent, listen to pipe         
+		while 1:             
+			line = os.read(pipein, 32)           # blocks until data sent 
+			# print('Read from Pipeline')
+			line = line.decode('utf-8') 
+			print(line)           
+			time.sleep(1)
+			# print(f'Parent {os.getpid()} got {line} at {time.time( )}')
+
+parent( ) 
